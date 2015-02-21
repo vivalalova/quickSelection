9d00979ae5f45a815de0ff51118c0f59e0370163
ShihKuo-Hsun <vivalalova0@gmail.com>
2015-02-21 22:25:39 +0800

    Initial Commit

---
+++ quickSelection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  quickSelection
+//
+//  Created by ShihKuo-Hsun on 2015/2/21.
+//  Copyright (c) 2015年 LO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ quickSelection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  quickSelection
+//
+//  Created by ShihKuo-Hsun on 2015/2/21.
+//  Copyright (c) 2015年 LO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
